0a0c8b8ec25d4c1186c5ea5ed5bfec0983a0ed80
Jean <38891630+skycod3@users.noreply.github.com>
2023-08-14 08:37:27 -0300

    Add files via upload

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+interface CardProps {
+  title: string;
+}
+
+export default function Card(props: CardProps) {
+  return <div className="p-10 mx-4 bg-neutral-200 rounded grid place-items-center">{props.title}</div>;
+}
